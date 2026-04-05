e36fa71afcfe494c037690554872718a1df513a2
Ven Corwell <vencorwell@gmail.com>
2026-04-05 15:24:49 -0400

    feat: Added Account Class

---
+++ banking101/src/main/java/Account.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Account {
+    public enum AccountType{
+        Checking,
+        Savings
+    }
+
+    private long accountNumber;
+    private AccountType accountType;
+    private String accountHolderName;
+    private double balance;
+
+    public Account(long accountNumber, AccountType accountType, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+}
